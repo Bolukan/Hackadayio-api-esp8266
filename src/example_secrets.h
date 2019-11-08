8161344d25e7e453fe681308e9bfdb9a5dc71dc5
Bolukan <github@bolukan.nl>
2019-11-08 23:14:22 +0100

    Example secrets.h

---
+++ src/example_secrets.h
@@ -0,0 +1,10 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+const char WIFI_SSID2[] = "SSID_MAIN";
+const char WIFI_PASSWORD2[] = "PASSWORD_MAIN";
+
+const char WIFI_SSID1[] = "SSID_BACKUP";
+const char WIFI_PASSWORD1[] = "PASSWORD_BACKUP";
+
+const char HACKADAYIO_API_KEY[] = "1234567890123456";
+#endif
